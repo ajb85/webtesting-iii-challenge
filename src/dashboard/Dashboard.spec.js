3521cccc6bff9246ad7d3c8a14a3af9097b56d9d
ajb85 <ajbrush85@gmail.com>
2019-04-10 15:37:58 -0400

    Verifies Dashboard renders without crashes and matches the snapshot

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+import react from "React";
+import ReactDOM from "react-dom";
+import { render, fireEvent } from "react-testing-library";
+import renderer from "react-test-renderer";
+import "jest-dom/extend-expect";
+
+import Dashboard from "./Dashboard.js";
+
+describe("<Dashboard />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
